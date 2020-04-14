2fd6bcfeb5faf1a223a3ca6a390441f9da355953
Brandon Williams <bwilliams.eng@gmail.com>
2020-04-13 22:33:06 -0700

    add documentation and examples

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,42 @@ static SWITCHBOARD: Lazy<Mutex<SwitchBoard>> =
 
 struct SwitchBoard(HashMap<NonZeroU16, Sender<MemorySocket>>, u16);
 
+/// An in-memory socket server, listening for connections.
+///
+/// After creating a `MemoryListener` by [`bind`]ing it to a socket address, it listens
+/// for incoming connections. These can be accepted by calling [`accept`] or by iterating over
+/// iterating over the [`Incoming`] iterator returned by [`incoming`][`MemoryListener::incoming`].
+///
+/// The socket will be closed when the value is dropped.
+///
+/// [`accept`]: #method.accept
+/// [`bind`]: #method.bind
+/// [`Incoming`]: struct.Incoming.html
+/// [`MemoryListener::incoming`]: #method.incoming
+///
+/// # Examples
+///
+/// ```no_run
+/// use std::io::{Read, Result, Write};
+///
+/// use memory_socket::{MemoryListener, MemorySocket};
+///
+/// fn write_stormlight(mut stream: MemorySocket) -> Result<()> {
+///     let msg = b"The most important step a person can take is always the next one.";
+///     stream.write_all(msg)?;
+///     stream.flush()
+/// }
+///
+/// fn main() -> Result<()> {
+///     let mut listener = MemoryListener::bind(16)?;
+///
+///     // accept connections and process them serially
+///     for stream in listener.incoming() {
+///         write_stormlight(stream?)?;
+///     }
+///     Ok(())
+/// }
+/// ```
 pub struct MemoryListener {
     incoming: Receiver<MemorySocket>,
     port: NonZeroU16,
@@ -28,6 +64,28 @@ impl Drop for MemoryListener {
 }
 
 impl MemoryListener {
+    /// Creates a new `MemoryListener` which will be bound to the specified
+    /// port.
+    ///
+    /// The returned listener is ready for accepting connections.
+    ///
+    /// Binding with a port number of `0` will request that a port be assigned
+    /// to this listener. The port allocated can be queried via the
+    /// [`local_addr`] method.
+    ///
+    /// [`local_addr`]: #method.local_addr
+    ///
+    /// # Examples
+    ///
+    /// Create a MemoryListener bound to port 16:
+    ///
+    /// ```no_run
+    /// use memory_socket::MemoryListener;
+    ///
+    /// # fn main () -> ::std::io::Result<()> {
+    /// let listener = MemoryListener::bind(16)?;
+    /// # Ok(())}
+    /// ```
     pub fn bind(port: u16) -> Result<Self> {
         let mut switchboard = (&*SWITCHBOARD).lock().unwrap();
 
@@ -70,19 +128,89 @@ impl MemoryListener {
         })
     }
 
+    /// Returns the local address that this listener is bound to.
+    ///
+    /// This can be useful, for example, when binding to port `0` to figure out
+    /// which port was actually bound.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use memory_socket::MemoryListener;
+    ///
+    /// # fn main () -> ::std::io::Result<()> {
+    /// let listener = MemoryListener::bind(16)?;
+    ///
+    /// assert_eq!(listener.local_addr(), 16);
+    /// # Ok(())}
+    /// ```
     pub fn local_addr(&self) -> u16 {
         self.port.get()
     }
 
+    /// Returns an iterator over the connections being received on this
+    /// listener.
+    ///
+    /// The returned iterator will never return `None`. Iterating over
+    /// it is equivalent to calling [`accept`] in a loop.
+    ///
+    /// [`accept`]: #method.accept
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::io::{Read, Result, Write};
+    ///
+    /// use memory_socket::MemoryListener;
+    ///
+    /// let mut listener = MemoryListener::bind(80).unwrap();
+    ///
+    /// for stream in listener.incoming() {
+    ///     match stream {
+    ///         Ok(stream) => {
+    ///             println!("new client!");
+    ///         }
+    ///         Err(e) => { /* connection failed */ }
+    ///     }
+    /// }
+    /// ```
     pub fn incoming(&mut self) -> Incoming<'_> {
         Incoming { inner: self }
     }
 
-    fn accept(&mut self) -> Option<MemorySocket> {
-        self.incoming.iter().next()
+    /// Accept a new incoming connection from this listener.
+    ///
+    /// This function will block the calling thread until a new connection
+    /// is established. When established, the corresponding [`MemorySocket`]
+    /// will be returned.
+    ///
+    /// [`MemorySocket`]: struct.MemorySocket.html
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::net::TcpListener;
+    /// use memory_socket::MemoryListener;
+    ///
+    /// let mut listener = MemoryListener::bind(8080).unwrap();
+    /// match listener.accept() {
+    ///     Ok(_socket) => println!("new client!"),
+    ///     Err(e) => println!("couldn't get client: {:?}", e),
+    /// }
+    /// ```
+    pub fn accept(&mut self) -> Result<MemorySocket> {
+        self.incoming.iter().next().ok_or_else(|| unreachable!())
     }
 }
 
+/// An iterator that infinitely [`accept`]s connections on a [`MemoryListener`].
+///
+/// This `struct` is created by the [`incoming`] method on [`MemoryListener`].
+/// See its documentation for more info.
+///
+/// [`accept`]: struct.MemoryListener.html#method.accept
+/// [`incoming`]: struct.MemoryListener.html#method.incoming
+/// [`MemoryListener`]: struct.MemoryListener.html
 pub struct Incoming<'a> {
     inner: &'a mut MemoryListener,
 }
@@ -91,10 +219,38 @@ impl<'a> Iterator for Incoming<'a> {
     type Item = Result<MemorySocket>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.inner.accept().map(|socket| Ok(socket))
+        Some(self.inner.accept())
     }
 }
 
+/// An in-memory stream between two local sockets.
+///
+/// A `MemorySocket` can either be created by connecting to an endpoint, via the
+/// [`connect`] method, or by [accepting] a connection from a [listener].
+/// It can be read or written to using the `Read` and `Write` traits.
+///
+/// # Examples
+///
+/// ```
+/// use std::io::{Read, Result, Write};
+/// use memory_socket::MemorySocket;
+///
+/// # fn main() -> Result<()> {
+/// let (mut socket_a, mut socket_b) = MemorySocket::new_pair();
+///
+/// socket_a.write_all(b"stormlight")?;
+/// socket_a.flush()?;
+///
+/// let mut buf = [0; 10];
+/// socket_b.read_exact(&mut buf)?;
+/// assert_eq!(&buf, b"stormlight");
+///
+/// # Ok(())}
+/// ```
+///
+/// [`connect`]: struct.MemorySocket.html#method.connect
+/// [accepting]: struct.MemoryListener.html#method.accept
+/// [listener]: struct.MemoryListener.html
 pub struct MemorySocket {
     incoming: Receiver<Bytes>,
     outgoing: Sender<Bytes>,
@@ -112,6 +268,15 @@ impl MemorySocket {
         }
     }
 
+    /// Construct both sides of an in-memory socket.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use memory_socket::MemorySocket;
+    ///
+    /// let (socket_a, socket_b) = MemorySocket::new_pair();
+    /// ```
     pub fn new_pair() -> (Self, Self) {
         let (a_tx, a_rx) = flume::unbounded();
         let (b_tx, b_rx) = flume::unbounded();
@@ -121,7 +286,22 @@ impl MemorySocket {
         (a, b)
     }
 
-    pub fn connet(port: u16) -> Result<MemorySocket> {
+    /// Create a new in-memory Socket connected to the specified port.
+    ///
+    /// This function will create a new MemorySocket socket and attempt to connect it to
+    /// the `port` provided.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use memory_socket::MemorySocket;
+    ///
+    /// # fn main () -> ::std::io::Result<()> {
+    /// # let _listener = memory_socket::MemoryListener::bind(16)?;
+    /// let socket = MemorySocket::connect(16)?;
+    /// # Ok(())}
+    /// ```
+    pub fn connect(port: u16) -> Result<MemorySocket> {
         let mut switchboard = (&*SWITCHBOARD).lock().unwrap();
 
         // Find port to connect to
